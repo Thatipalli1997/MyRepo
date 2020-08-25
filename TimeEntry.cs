d73b02216c787a9a002928572bee8895d5989ec1
Thatipalli <ruchitha.thatipalli@dxc.com>
2020-08-25 09:46:23 +0530

    FirstCommit

---
+++ TimeEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeEntryApiApplication
+{
+    public class TimeEntry
+    {
+        public long? Id { get; set; }
+        public long ProjectId { get; set; }
+        public long UserId { get; set; }
+        public DateTime Date { get; set; }
+        public int Hours { get; set; }
+
+
+        public TimeEntry(long id, long projectid, long userId, DateTime date, int hours)
+        {
+            Id = id;
+            ProjectId = projectid;
+            UserId = userId;
+            Date = date;
+            Hours = hours;
+
+        }
+        public TimeEntry(long projectid, long userid, DateTime date, int hours)
+        {
+            Id = null;
+            ProjectId = projectid;
+            UserId = userid;
+            Date = date;
+            Hours = hours;
+
+        }
+
+    }
+}
